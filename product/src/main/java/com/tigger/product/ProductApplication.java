e6ffe8b8a756783cdce86d34d512fac32ac3d4af
hsl <hsl3757@gmail.com>
2021-11-23 21:32:11 +0900

    test commit

--- product/src/main/java/com/tigger/product/ProductApplication.java
+++ product/src/main/java/com/tigger/product/ProductApplication.java
@@ -11,5 +11,7 @@ public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
         log.info("##########ProductApplication start!");
+
+        log.info("test");
     }
 }
