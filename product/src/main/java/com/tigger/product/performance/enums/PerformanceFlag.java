a445a1cd8f3adb878d55a000df73812c50c20bed
suyeon0 <83405795+suyeon0@users.noreply.github.com>
2021-12-03 23:48:44 +0900

    reformat code 건

--- product/src/main/java/com/tigger/product/performance/enums/PerformanceFlag.java
+++ product/src/main/java/com/tigger/product/performance/enums/PerformanceFlag.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PerformanceFlag {
-    NORMAL("정상공연","0"),
-    CANCEL("취소공연","1"),
-    END("종료공연","2");
+    NORMAL("정상공연", "0"),
+    CANCEL("취소공연", "1"),
+    END("종료공연", "2");
 
     private String status;
     private String value;
