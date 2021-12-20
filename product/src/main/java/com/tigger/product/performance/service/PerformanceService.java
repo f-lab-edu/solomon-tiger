affa5c4d76d0a0e229cf9849fba5c0e4673fdc56
suyeon0 <83405795+suyeon0@users.noreply.github.com>
2021-12-20 22:52:51 +0900

    style: 주석 bug: 공연검색 결과 다수일 때

--- product/src/main/java/com/tigger/product/performance/service/PerformanceService.java
+++ product/src/main/java/com/tigger/product/performance/service/PerformanceService.java
@@ -2,12 +2,14 @@ package com.tigger.product.performance.service;
 
 import com.tigger.product.performance.dao.PerformanceMapper;
 import com.tigger.product.performance.dto.PerformanceDTO;
-import com.tigger.product.performance.enums.PerformanceFlag;
+import com.tigger.product.performance.enums.PerformanceState;
 import com.tigger.product.performance.exception.CustomRuntimeException;
 import com.tigger.product.performance.exception.DupPerformanceException;
 import com.tigger.product.performance.exception.NonExistentPerformanceException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 @RequiredArgsConstructor
@@ -21,19 +23,22 @@ public class PerformanceService {
      * @param dto
      */
     public void chkDupPerformance(PerformanceDTO dto) {
-        int dupCnt = pMapper.getDupPerformanceCnt(dto.getEnterpriseCode(), dto.getEnterprisePid());
+        Assert.notNull(dto.getEnterpriseCode(), "업체코드가 존재하지 않습니다");
+        Assert.notNull(dto.getEnterprisePid(), "업체상품코드가 존재하지 않습니다");
+        int dupCnt = this.pMapper.getDupPerformanceCnt(dto.getEnterpriseCode(), dto.getEnterprisePid());
         if (dupCnt > 0) {
             throw new DupPerformanceException();
         }
     }
 
     /**
-     * 신규 공연 입력
+     * 신규 공연 등록
      *
      * @param performance
      */
     public void registerPerformance(PerformanceDTO performance) {
-        int result = pMapper.insertPerformance(performance);
+        Assert.notNull(performance, "Performance is null");
+        int result = this.pMapper.insertPerformance(performance);
         if (result == 0) {
             throw new CustomRuntimeException("registerPerformance exception 발생");
         }
@@ -42,11 +47,12 @@ public class PerformanceService {
     /**
      * 공연 조회(id 기준)
      *
-     * @param id
-     * @return
+     * @param id : 공연 ID
+     * @return PerformanceDTO
      */
     public PerformanceDTO getPerformanceById(int id) {
-        PerformanceDTO result = pMapper.getById(id);
+        Assert.notNull(id, "ID is null");
+        PerformanceDTO result = this.pMapper.getById(id);
         if (result == null) {
             throw new NonExistentPerformanceException();
         }
@@ -56,26 +62,40 @@ public class PerformanceService {
     /**
      * 공연 조회(공연명 기준)
      *
-     * @param name
-     * @return
+     * @param name : 공연명 검색 키워드
+     * @return List<PerformanceDTO> : 검색 결과 리스트
      */
-    public PerformanceDTO getPerformanceByName(String name) {
-        return pMapper.getByName(name);
+    private List<PerformanceDTO> getPerformanceListByName(String name) {
+        return this.pMapper.getListByName(name);
     }
 
     /**
      * 공연 상태값 변경( 정상등록 -> 취소 )
      *
-     * @param id
+     * @param id : 공연 ID
      */
-    public void updateFlag(int id) {
-        if (PerformanceFlag.CANCEL.getValue().equals(pMapper.getCurrentFlag(id))) {
+    public void updateState(int id) {
+        Assert.notNull(id, "ID is null");
+        if (PerformanceState.CANCEL.getValue().equals(this.pMapper.getCurrentState(id))) {
             throw new IllegalArgumentException("취소 공연 상태값 변경 불가");
         }
-        if (pMapper.chkExistPaymentCnt(id) > 0) {
+        if (this.pMapper.chkExistPaymentCnt(id) > 0) {
             throw new CustomRuntimeException("유효 결제건 존재하여 취소 불가");
         }
-        pMapper.updateFlag(id); //해당 공연 취소
+        this.pMapper.updateState(id);
+        this.validForUpdateState(id);
+    }
+
+    /**
+     * updateState 결과에 대한 DB 데이터 검증
+     *
+     * @param id : 공연 ID
+     */
+    private void validForUpdateState(int id) {
+        String dbVal = this.pMapper.getCurrentState(id);
+        if (!PerformanceState.CANCEL.getValue().equals(dbVal)) {
+            throw new CustomRuntimeException("공연 상태값 변경이 정상적으로 이루어지지 않았습니다");
+        }
     }
 
 }
